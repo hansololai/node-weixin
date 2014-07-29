ec0bf15860d7914c37193654027399d200c07784
Evilsantac <han.lai321@gmail.com>
2014-07-29 01:21:28 -0400

    client side

---
+++ AzuneProjectWeixin/Client/View.js
@@ -0,0 +1,77 @@
+﻿var $ = require('jquery')(window);
+var Backbone = require('backbone');
+var template = require('./template/message.hbs');
+
+SettingView = Backbone.View.extend({
+    initialize: function (option) {
+        $(".settings-content").removeClass('active');
+        this.sidebar = new Sidebar({
+            el: '.settings-sidebar',
+            pane: options.pane,
+            model:this.model
+        });
+        this.listenTo(router, 'route:settings', this.changePane);
+    },
+    changePane: function (pane) {
+        if (!pane) {
+            return;
+        }
+        this.sidebar.showContent(pane);
+    },
+    render: function(){
+        this.$el.html(template(this.collection.toJSON()));
+    }
+    
+});
+Sidebar = Backbone.View.extend({
+    initialize: function (option) {
+        this.render();
+        this.menu = this.$('.settings-menu');
+        this.showContent(options.pane);
+    },
+    models: {},
+    events: {
+        'click .settings-menu li': 'switchPane'
+    },
+    switchPane: function (e) {
+        e.preventDefault();
+        var item = $(e.currentTarget),
+            id = item.find('a').attr('href').substring(1);
+
+        this.showContent(id);
+    },
+
+    showContent: function (id) {
+        var self = this,
+            model;
+
+        Ghost.router.navigate('/settings/' + id + '/');
+        Ghost.trigger('urlchange');
+        if (this.pane && id === this.pane.id) {
+            return;
+        }
+        _.result(this.pane, 'destroy');
+        this.setActive(id);
+        this.pane = new Settings[id]({ el: '.settings-content' });
+
+        if (!this.models.hasOwnProperty(this.pane.options.modelType)) {
+            model = this.models[this.pane.options.modelType] = new Ghost.Models[this.pane.options.modelType]();
+            model.fetch().then(function () {
+                self.renderPane(model);
+            });
+        } else {
+            model = this.models[this.pane.options.modelType];
+            self.renderPane(model);
+        }
+    },
+
+    renderPane: function (model) {
+        this.pane.model = model;
+        this.pane.render();
+    },
+
+    setActive: function (id) {
+        this.menu.find('li').removeClass('active');
+        this.menu.find('a[href=#' + id + ']').parent().addClass('active');
+    }
+});
---
+++ AzuneProjectWeixin/Client/models.js
@@ -0,0 +1,20 @@
+﻿var Backbone = require('backbone');
+$ = require('jquery')(window);
+
+Message = Backbone.Model.extend({
+    initialize: function () {
+        
+    },
+    defaults: {
+        idMessage:0,
+        FromUserName: '',
+        ToUserName: '',
+        Content: '',
+        ReplyFor:0
+    },
+    restUrl:'/api/'
+
+});
+MessageCollection = Backbone.Collection.extend({
+    model:Message,
+});
