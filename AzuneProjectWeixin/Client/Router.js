4613f92f1e557c706470f4d940e381f5b4b97972
Han <han@ubuntu.(none)>
2014-07-30 18:55:35 -0400

    added browserify

---
+++ AzuneProjectWeixin/Client/Router.js
@@ -0,0 +1,34 @@
+/**
+ * New node file
+ */
+"use strict";
+var $ = require('jquery');
+var Backbone = require('backbone');
+Backbone.$=$;
+var View=require('./View');
+var Router=Backbone.Router.extend({
+	routes: {
+        'settings(/:pane)/' : 'settings',
+        
+    },
+    settings: function (pane) {
+        if (!pane) {
+        	alert('redirect');
+            // Redirect to settings/general if no pane supplied
+            this.navigate('/settings/general/', {
+                trigger: true,
+                replace: true,
+            });
+            return;
+        }
+
+        myApp.currentView=new View.Setting({el:'#main',pane:pane});
+        // only update the currentView if we don't already have a Settings view
+//        if (!Ghost.currentView || !(Ghost.currentView instanceof Ghost.Views.Settings)) {
+//            Ghost.currentView = new Ghost.Views.Settings({ el: '#main', pane: pane });
+//        }
+        myApp.currentView.render();
+    }
+	
+});
+module.exports=Router;
